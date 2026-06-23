2e430b9d02295a26c8395f4591da97feddb952d8
Alafandri <131587531+Alafandri@users.noreply.github.com>
2026-06-23 23:26:46 +0300

    Controls
    Made characters controls better

---
+++ Code/player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -600.0
+
+const JUMP_BUFFER_TIME = 0.1
+var jump_buffer_timer = 0.0
+
+@onready var sprite = $Sprite2D
+
+const FALL_GRAVITY := 2000.0
+
+func get_custom_gravity(current_velocity: Vector2) -> float:
+	if current_velocity.y < 0:
+		return get_gravity().y
+	return FALL_GRAVITY
+
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_axis("Left", "Right")
+	
+	if direction != 0:
+		sprite.flip_h = direction < 0
+	
+	if not is_on_floor():
+		velocity.y += get_custom_gravity(velocity) * delta
+
+	if Input.is_action_just_released("Jump") and velocity.y < 0:
+		velocity.y = JUMP_VELOCITY / 4
+
+	if Input.is_action_just_pressed("Jump"):
+		jump_buffer_timer = JUMP_BUFFER_TIME
+	else:
+		jump_buffer_timer -= delta
+
+	if is_on_floor() and jump_buffer_timer > 0:
+		velocity.y = JUMP_VELOCITY
+		jump_buffer_timer = 0.0
+
+	if direction != 0:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
